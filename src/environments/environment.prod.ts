33e7c33478b337dde2ca1b946eb96f93d669ed1d
ricardodavivi <65041928+ricardodavivi@users.noreply.github.com>
2020-10-22 16:09:37 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   
-  api: 'http://visasupermercados-app.umbler.net'
+  api: 'https://visasupermercados-app.umbler.net'
   //api:'http://localhost:3000'
   
 };
