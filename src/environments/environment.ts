a618cec6a16206bbb7459ad9b69bf79357ba93f8
ricardodavivi <65041928+ricardodavivi@users.noreply.github.com>
2020-10-22 16:27:51 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   
-  api: 'http://api-meuapp-com.umbler.net'
+  api: 'https://api-meuapp-com.umbler.net'
   //api:'http://localhost:3000'
   
 };
